862fca9c9822e96b8c9cb3111106fe201818fa08
Sam Edwards <139229455+SamEdwardsDev@users.noreply.github.com>
2023-10-06 10:31:25 +0100

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -11,13 +11,15 @@
 
 // This is a javacsript Comment
 
-const sam = {
-  firstName: 'sam',
-  lastName: 'Edwards',
-  born: 1994,
+// Hamburger Menu
 
-  ageCalc: function () {
-    this.age = 2023 - this.born;
-    return this.age;
-  },
-};
+const hamburger = document.querySelector('.nav__hamburger');
+const hamburgerImg = document.querySelector('.nav__hamburger__img');
+const mobNav = document.querySelector('.mobile__nav');
+
+hamburger.addEventListener('click', function () {
+  mobNav.classList.toggle('active');
+  mobNav.classList.contains('active')
+    ? (hamburgerImg.src = 'assets/img/Menu/Close_round_light')
+    : (hamburgerImg.src = 'assets/img/Menu/Menu_Alt_05.png');
+});
