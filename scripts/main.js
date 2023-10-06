5f078ef9194b519e836ee89a4f187f28474196b6
Sam Edwards <139229455+SamEdwardsDev@users.noreply.github.com>
2023-10-06 10:03:27 +0100

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+// const navbar = document.querySelector('.nav');
+// window.onscroll = () => {
+//   if (window.scrollY > 150) {
+//     navbar.classList.add('scrolled');
+//     navbar.classList.remove('transparent');
+//   } else {
+//     navbar.classList.add('transparent');
+//     navbar.classList.remove('scrolled');
+//   }
+// };
+
+// This is a javacsript Comment
+
+const sam = {
+  firstName: 'sam',
+  lastName: 'Edwards',
+  born: 1994,
+
+  ageCalc: function () {
+    this.age = 2023 - this.born;
+    return this.age;
+  },
+};
